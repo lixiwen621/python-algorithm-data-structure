75e2b40d93903c77ab67357b256c308beb004fe6
lixiwen621 <592864759@qq.com>
2025-01-14 16:27:53 +0800

    Commit compute_factorial

---
+++ math/fact_gen.py
@@ -0,0 +1,28 @@
+# 阶乘
+def prod(a,b):
+    return a * b
+
+
+def fact_gen():
+    i = 1
+    n = i
+    while True:
+        output = prod(n, i)
+        yield output
+        i += 1
+        n = output
+
+
+def compute_factorial(num):
+    if not isinstance(num,int):
+        raise ValueError("Please pass the number")
+    result = 0
+    gen = fact_gen()
+    for n in range(num):
+        result = next(gen)
+    return result
+
+
+if __name__ == '__main__':
+    fact = compute_factorial(5)
+    print(fact)
